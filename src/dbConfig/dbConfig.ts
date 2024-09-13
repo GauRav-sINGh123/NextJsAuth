083e88441d19a421b723ebbaed1fd6943394df12
Gaurav Singh <gksingh947@gmail.com>
2024-09-13 21:58:24 +0530

    Auth

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+         await mongoose.connect(process.env.MONGO_URI!);
+         const connection = mongoose.connection;
+         connection.on("connected", () => {
+            console.log("MongoDB connected");
+        })
+
+        connection.on("error", (error) => {
+            console.error(`MongoDB connection error please check:`);
+            process.exit();
+        })
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        process.exit();
+    }
+}
