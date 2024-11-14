a5dba0d7bc32cec5b798ad4ccd774232f3a10f05
Gaurav Singh <gksingh947@gmail.com>
2024-11-14 23:08:12 +0530

    User Profile Create

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+export default function Profile() {
+ 
+  const user = {
+    username: "JohnDoe",
+    email: "john.doe@example.com"
+  }
 
-function Profile() {
   return (
-    <div>Profile</div>
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-2xl">
+        <h2 className="text-3xl font-bold mb-6 text-black text-center">User Details</h2>
+        <div className="space-y-4">
+          <div>
+            <h3 className="text-lg font-semibold text-gray-900">Username</h3>
+            <p className="mt-1 text-gray-700">{user.username}</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold text-gray-900">Email</h3>
+            <p className="mt-1 text-gray-700">{user.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-export default Profile
