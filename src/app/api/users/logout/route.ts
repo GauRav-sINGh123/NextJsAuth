2e4773d1acf45fca44fd942e21f06d151ec0a7d3
Gaurav Singh <gksingh947@gmail.com>
2024-11-14 21:19:01 +0530

    About Route Create

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,20 +1,18 @@
-import { connectDB } from "@/dbConfig/dbConfig";
-import User from "@/models/userModel";
-import { NextResponse, NextRequest } from "next/server";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-
-connectDB()
+ import { NextResponse, NextRequest } from "next/server";
 
 
 export async function GET(request: NextRequest) {
     try {
-        const response=NextResponse.json({message:"Logout successful",success:true})
-        response.cookies.set("token","",{
-            httpOnly:true,
-            expires:new Date(0)
-        })
-
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
+        });
+        return response;
     } catch (error:any) {
         return NextResponse.json({err:error.message},{status:500})
     }
